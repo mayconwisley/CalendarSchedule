9291ae858332f71bb6fd5dc1c04b94b19e5ca408
Maycon Batista <maycon.batista@seniornortepr.com.br>
2023-09-01 14:14:22 -0300

    Ajuste

--- AgendaSalas.Web/Service/IAgendaService.cs
+++ AgendaSalas.Web/Service/IAgendaService.cs
@@ -6,6 +6,7 @@ namespace AgendaSalas.Web.Service;
 public interface IAgendaService
 {
     Task<AgendaView> GetAll(int page = 1, int size = 25, string search = "");
+    Task<IEnumerable<AgendaDto>> GetByDate(DateTime dateTime);
     Task<AgendaDto> GetById(int id);
     Task<AgendaDto> Create(AgendaDto agendaDto);
     Task<AgendaDto> Update(AgendaDto agendaDto);
