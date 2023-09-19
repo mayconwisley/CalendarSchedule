be183f8e220cb3897de0a931a1bdb412637d3dc4
Maycon Batista <maycon.batista@seniornortepr.com.br>
2023-09-19 14:47:17 -0300

    Ajuste

--- ScheduleRooms.Web/Service/ScheduleService.cs
+++ ScheduleRooms.Web/Service/ScheduleService.cs
@@ -6,211 +6,210 @@ using System.Text;
 using System.Text.Json;
 using System.Drawing;
 
-namespace ScheduleRooms.Web.Service
+namespace ScheduleRooms.Web.Service;
+
+public class ScheduleService : IScheduleService
 {
-    public class ScheduleService : IScheduleService
+    private readonly IHttpClientFactory _httpClientFactory;
+    private readonly JsonSerializerOptions _serializerOptions;
+    private const string? apiEndPoint = "api/Schedule";
+
+    public ScheduleService(IHttpClientFactory httpClientFactory)
     {
-        private readonly IHttpClientFactory _httpClientFactory;
-        private readonly JsonSerializerOptions _serializerOptions;
-        private const string? apiEndPoint = "api/Schedule";
+        _httpClientFactory = httpClientFactory;
+        _serializerOptions = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+    }
 
-        public ScheduleService(IHttpClientFactory httpClientFactory)
+    public async Task<ScheduleDto> Create(ScheduleDto scheduleDto)
+    {
+        try
         {
-            _httpClientFactory = httpClientFactory;
-            _serializerOptions = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
-        }
+            using var httpClient = _httpClientFactory.CreateClient("ConexaoApi");
 
-        public async Task<ScheduleDto> Create(ScheduleDto scheduleDto)
-        {
-            try
+            StringContent stringContent = new(JsonSerializer.Serialize(scheduleDto), Encoding.UTF8, "application/json");
+
+            using (var response = await httpClient.PostAsync(apiEndPoint, stringContent))
             {
-                using var httpClient = _httpClientFactory.CreateClient("ConexaoApi");
-
-                StringContent stringContent = new(JsonSerializer.Serialize(scheduleDto), Encoding.UTF8, "application/json");
-
-                using (var response = await httpClient.PostAsync(apiEndPoint, stringContent))
-                {
-                    if (response.IsSuccessStatusCode)
-                    {
-                        using Stream resApi = await response.Content.ReadAsStreamAsync();
-                        var schedule = await JsonSerializer.DeserializeAsync<ScheduleDto>(resApi, _serializerOptions);
-                        if (schedule is not null)
-                        {
-                            return schedule;
-                        }
-                    }
-                    if (response.StatusCode == HttpStatusCode.Conflict)
-                    {
-                        Console.Out.WriteLine("Erro");
-                    }
-                }
-                return new();
-            }
-            catch (Exception)
-            {
-                throw;
-            }
-        }
-
-        public async Task<bool> Delete(int id)
-        {
-            try
-            {
-                using var httpClient = _httpClientFactory.CreateClient("ConexaoApi");
-                using var response = await httpClient.DeleteAsync($"{apiEndPoint}/{id}");
                 if (response.IsSuccessStatusCode)
                 {
-                    return true;
+                    using Stream resApi = await response.Content.ReadAsStreamAsync();
+                    var schedule = await JsonSerializer.DeserializeAsync<ScheduleDto>(resApi, _serializerOptions);
+                    if (schedule is not null)
+                    {
+                        return schedule;
+                    }
                 }
-                return new();
+                if (response.StatusCode == HttpStatusCode.Conflict)
+                {
+                    throw new Exception("Informações conflitantes");
+                }
             }
-            catch (Exception)
-            {
-                throw;
-            }
+            return new();
         }
-
-        public async Task<ScheduleView> GetAll(int page = 1, int size = 10, string search = "")
+        catch (Exception)
         {
-            try
+            throw;
+        }
+    }
+
+    public async Task<bool> Delete(int id)
+    {
+        try
+        {
+            using var httpClient = _httpClientFactory.CreateClient("ConexaoApi");
+            using var response = await httpClient.DeleteAsync($"{apiEndPoint}/{id}");
+            if (response.IsSuccessStatusCode)
             {
-                using var httpClient = _httpClientFactory.CreateClient("ConexaoApi");
-                using var response = await httpClient.GetAsync($"{apiEndPoint}/All?page={page}&size={size}&search={search}");
+                return true;
+            }
+            return new();
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+    }
+
+    public async Task<ScheduleView> GetAll(int page = 1, int size = 10, string search = "")
+    {
+        try
+        {
+            using var httpClient = _httpClientFactory.CreateClient("ConexaoApi");
+            using var response = await httpClient.GetAsync($"{apiEndPoint}/All?page={page}&size={size}&search={search}");
 
 
+            if (response.IsSuccessStatusCode)
+            {
+                var scheduleView = await response.Content.ReadFromJsonAsync<ScheduleView>(_serializerOptions);
+                return scheduleView ?? new();
+            }
+            else
+            {
+                if (response.StatusCode == HttpStatusCode.NotFound)
+                {
+                    return new();
+                }
+                response.EnsureSuccessStatusCode();
+            }
+            return new();
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+    }
+
+    public async Task<ScheduleDto> GetById(int id)
+    {
+        try
+        {
+            using var httpClient = _httpClientFactory.CreateClient("ConexaoApi");
+            using var response = await httpClient.GetAsync($"{apiEndPoint}/{id}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var scheduleDto = await response.Content.ReadFromJsonAsync<ScheduleDto>(_serializerOptions);
+                return scheduleDto ?? new();
+            }
+            else
+            {
+                if (response.StatusCode == HttpStatusCode.NotFound)
+                {
+                    return new();
+                }
+                response.EnsureSuccessStatusCode();
+            }
+            return new();
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+    }
+
+    public async Task<ScheduleDto> Update(ScheduleDto scheduleDto)
+    {
+        try
+        {
+            using var httpClient = _httpClientFactory.CreateClient("ConexaoApi");
+
+            StringContent stringContent = new(JsonSerializer.Serialize(scheduleDto), Encoding.UTF8, "application/json");
+
+            using (var response = await httpClient.PutAsync($"{apiEndPoint}/{scheduleDto.Id}", stringContent))
+            {
                 if (response.IsSuccessStatusCode)
                 {
-                    var scheduleView = await response.Content.ReadFromJsonAsync<ScheduleView>(_serializerOptions);
-                    return scheduleView ?? new();
-                }
-                else
-                {
-                    if (response.StatusCode == HttpStatusCode.NotFound)
+                    using Stream resApi = await response.Content.ReadAsStreamAsync();
+                    var schedule = await JsonSerializer.DeserializeAsync<ScheduleDto>(resApi, _serializerOptions);
+                    if (schedule is not null)
                     {
-                        return new();
+                        return schedule;
                     }
-                    response.EnsureSuccessStatusCode();
                 }
-                return new();
             }
-            catch (Exception)
-            {
-                throw;
-            }
+            return new();
         }
-
-        public async Task<ScheduleDto> GetById(int id)
+        catch (Exception)
         {
-            try
-            {
-                using var httpClient = _httpClientFactory.CreateClient("ConexaoApi");
-                using var response = await httpClient.GetAsync($"{apiEndPoint}/{id}");
-
-                if (response.IsSuccessStatusCode)
-                {
-                    var scheduleDto = await response.Content.ReadFromJsonAsync<ScheduleDto>(_serializerOptions);
-                    return scheduleDto ?? new();
-                }
-                else
-                {
-                    if (response.StatusCode == HttpStatusCode.NotFound)
-                    {
-                        return new();
-                    }
-                    response.EnsureSuccessStatusCode();
-                }
-                return new();
-            }
-            catch (Exception)
-            {
-                throw;
-            }
+            throw;
         }
+    }
 
-        public async Task<ScheduleDto> Update(ScheduleDto scheduleDto)
+    public async Task<IEnumerable<ScheduleDto>> GetByAgendaActive()
+    {
+        try
         {
-            try
+            using var httpClient = _httpClientFactory.CreateClient("ConexaoApi");
+            using var response = await httpClient.GetAsync($"{apiEndPoint}/ScheduleActive");
+
+
+            if (response.IsSuccessStatusCode)
             {
-                using var httpClient = _httpClientFactory.CreateClient("ConexaoApi");
-
-                StringContent stringContent = new(JsonSerializer.Serialize(scheduleDto), Encoding.UTF8, "application/json");
-
-                using (var response = await httpClient.PutAsync($"{apiEndPoint}/{scheduleDto.Id}", stringContent))
+                var scheduleDto = await response.Content.ReadFromJsonAsync<IEnumerable<ScheduleDto>>(_serializerOptions);
+                return scheduleDto ??= new List<ScheduleDto>();
+            }
+            else
+            {
+                if (response.StatusCode == HttpStatusCode.NotFound)
                 {
-                    if (response.IsSuccessStatusCode)
-                    {
-                        using Stream resApi = await response.Content.ReadAsStreamAsync();
-                        var schedule = await JsonSerializer.DeserializeAsync<ScheduleDto>(resApi, _serializerOptions);
-                        if (schedule is not null)
-                        {
-                            return schedule;
-                        }
-                    }
+                    return new List<ScheduleDto>();
                 }
-                return new();
+                response.EnsureSuccessStatusCode();
             }
-            catch (Exception)
-            {
-                throw;
-            }
+            return new List<ScheduleDto>();
         }
-
-        public async Task<IEnumerable<ScheduleDto>> GetByAgendaActive()
+        catch (Exception)
         {
-            try
-            {
-                using var httpClient = _httpClientFactory.CreateClient("ConexaoApi");
-                using var response = await httpClient.GetAsync($"{apiEndPoint}/ScheduleActive");
-
-
-                if (response.IsSuccessStatusCode)
-                {
-                    var scheduleDto = await response.Content.ReadFromJsonAsync<IEnumerable<ScheduleDto>>(_serializerOptions);
-                    return scheduleDto ??= new List<ScheduleDto>();
-                }
-                else
-                {
-                    if (response.StatusCode == HttpStatusCode.NotFound)
-                    {
-                        return new List<ScheduleDto>();
-                    }
-                    response.EnsureSuccessStatusCode();
-                }
-                return new List<ScheduleDto>();
-            }
-            catch (Exception)
-            {
-                throw;
-            }
+            throw;
         }
+    }
 
-        public async Task<IEnumerable<ScheduleDto>> GetByAgendaActiveSalaId(int roomId, DateTime dateSalected)
+    public async Task<IEnumerable<ScheduleDto>> GetByAgendaActiveSalaId(int roomId, DateTime dateSalected)
+    {
+        try
         {
-            try
-            {
-                using var httpClient = _httpClientFactory.CreateClient("ConexaoApi");
-                using var response = await httpClient.GetAsync($"{apiEndPoint}/ScheduleActiveRoomId/{roomId}/{dateSalected.ToString("dd/MM/yyyy").Replace("/", "%2F")}");
+            using var httpClient = _httpClientFactory.CreateClient("ConexaoApi");
+            using var response = await httpClient.GetAsync($"{apiEndPoint}/ScheduleActiveRoomId/{roomId}/{dateSalected.ToString("dd/MM/yyyy").Replace("/", "%2F")}");
 
 
-                if (response.IsSuccessStatusCode)
-                {
-                    var scheduleView = await response.Content.ReadFromJsonAsync<IEnumerable<ScheduleDto>>(_serializerOptions);
-                    return scheduleView ??= new List<ScheduleDto>();
-                }
-                else
-                {
-                    if (response.StatusCode == HttpStatusCode.NotFound)
-                    {
-                        return new List<ScheduleDto>();
-                    }
-                    response.EnsureSuccessStatusCode();
-                }
-                return new List<ScheduleDto>();
-            }
-            catch (Exception)
+            if (response.IsSuccessStatusCode)
             {
-                throw;
+                var scheduleView = await response.Content.ReadFromJsonAsync<IEnumerable<ScheduleDto>>(_serializerOptions);
+                return scheduleView ??= new List<ScheduleDto>();
             }
+            else
+            {
+                if (response.StatusCode == HttpStatusCode.NotFound)
+                {
+                    return new List<ScheduleDto>();
+                }
+                response.EnsureSuccessStatusCode();
+            }
+            return new List<ScheduleDto>();
+        }
+        catch (Exception)
+        {
+            throw;
         }
     }
 }
