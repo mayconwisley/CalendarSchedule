551917c822655dacceee716cdd1cd2be57a81e9b
Maycon Wisley <maycon.wisley@outlook.com>
2023-11-16 20:59:15 -0300

    Ajuste

--- ScheduleRooms.Web/Program.cs
+++ ScheduleRooms.Web/Program.cs
@@ -7,10 +7,10 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-//var baseUrl = "https://localhost:7244";
+var baseUrl = "https://localhost:7244";
 
 //var baseUrl = "http://192.168.0.102:5050";
-var baseUrl = "http://192.168.10.149:5050";
+//var baseUrl = "http://192.168.10.149:5050";
 
 
 builder.Services.AddHttpClient("ConexaoApi", con =>
