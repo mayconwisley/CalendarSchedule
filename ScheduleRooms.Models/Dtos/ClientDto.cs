b4a7b6b618b05cb19746a138665fae37f50e442e
Maycon Batista <maycon.batista@seniornortepr.com.br>
2024-03-08 09:39:26 -0300

    Ajuste

--- ScheduleRooms.Models/Dtos/ClientDto.cs
+++ ScheduleRooms.Models/Dtos/ClientDto.cs
@@ -7,9 +7,11 @@ public class ClientDto
     public int Id { get; set; }
     [Required(ErrorMessage = "Nome obrigatório")]
     public string? Name { get; set; }
-    [Required(ErrorMessage = "Responsavel obrigatório")]
+    [Required(ErrorMessage = "Responsável obrigatório")]
     public string? Responsible { get; set; }
     [Required(ErrorMessage = "Telefone Obrigatório")]
+    [MaxLength(10, ErrorMessage = "Máximo 10 digito")]
+    [MinLength(10, ErrorMessage = "Mínino 10 digito")]
     public string? Telephone { get; set; }
     public string? Description { get; set; }
     public bool Active { get; set; } = true;
--- ScheduleRooms.Models/Dtos/UserDto.cs
+++ ScheduleRooms.Models/Dtos/UserDto.cs
@@ -8,6 +8,8 @@ public class UserDto
     [Required(ErrorMessage = "Nome obrigatório")]
     public string? Name { get; set; }
     [Required(ErrorMessage = "Celular obrigatório")]
+    [MaxLength(10, ErrorMessage = "Máximo 11 digito")]
+    [MinLength(10, ErrorMessage = "Mínino 11 digito")]
     public string? Cellphone { get; set; }
     public string? Description { get; set; }
     public bool Active { get; set; } = true;
