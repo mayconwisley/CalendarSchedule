406689fd6b33338713e7f97eb645f81fdd16d498
Maycon Batista <maycon.batista@seniornortepr.com.br>
2024-05-17 14:49:47 -0300

    Ajuste

--- ScheduleRooms.API/Repository/UserRepository.cs
+++ ScheduleRooms.API/Repository/UserRepository.cs
@@ -106,7 +106,7 @@ public class UserRepository(ScheduleContext scheduleContext) : IUserRepository
         try
         {
             IEnumerable<User> users = await _scheduleContext.Users
-                  .Where(w => w.Username!.Equals(username, StringComparison.CurrentCultureIgnoreCase))
+                  .Where(w => w.Username.ToUpper() == username.ToUpper())
                   .Skip((page - 1) * size)
                   .Take(size)
                   .ToListAsync();
@@ -134,7 +134,7 @@ public class UserRepository(ScheduleContext scheduleContext) : IUserRepository
         try
         {
             var user = await _scheduleContext.Users
-                .Where(w => w.Username!.Equals(username, StringComparison.CurrentCultureIgnoreCase))
+                .Where(w => w.Username.ToUpper() == username.ToUpper())
                 .FirstOrDefaultAsync();
 
             if (user is not null)
