60a408c5aea6c2e628758ce5742f6874bc39f316
Maycon Wisley <maycon.wisley@outlook.com>
2024-06-22 12:21:48 -0300

    Ajuste

--- ScheduleRooms.Models/Dtos/ClientContactDto.cs
+++ ScheduleRooms.Models/Dtos/ClientContactDto.cs
@@ -1,12 +1,21 @@
-﻿namespace ScheduleRooms.Models.Dtos;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ScheduleRooms.Models.Dtos;
 
 public class ClientContactDto
 {
     public int Id { get; set; }
+    [Required(ErrorMessage = "Tipo obrigatório")]
     public string? Type { get; set; }
+
+    [Required(ErrorMessage = "Numero obrigatório")]
     public string? Number { get; set; }
+
+    [Required(ErrorMessage = "ClientId obrigatório")]
     public int ClientId { get; set; }
     public ClientDto? ClientDto { get; set; }
+
+    [Required(ErrorMessage = "ResponsavelId obrigatório")]
     public int ClientResponsibleId { get; set; }
     public ClientResponsibleDto? ClientResponsibleDto { get; set; }
 }
