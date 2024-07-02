10c0e8c5e5c766a2a01c4f75e2cd421787f315a9
Maycon Batista <maycon.batista@seniornortepr.com.br>
2024-07-02 16:54:07 -0300

    Ajuste

---
+++ ScheduleRooms.Web/Service/Interface/IClientResponsibleService.cs
@@ -0,0 +1,13 @@
+﻿using ScheduleRooms.Models.Dtos;
+using ScheduleRooms.Web.Models;
+
+namespace ScheduleRooms.Web.Service.Interface;
+
+public interface IClientResponsibleService
+{
+    Task<ClientResponsibleView> GetAll(int page = 1, int size = 10, string search = "");
+    Task<ClientResponsibleDto> GetById(int id);
+    Task<ClientResponsibleDto> Create(ClientResponsibleDto clientResponsibleDto);
+    Task<ClientResponsibleDto> Update(ClientResponsibleDto clientResponsibleDto);
+    Task<bool> Delete(int id);
+}
