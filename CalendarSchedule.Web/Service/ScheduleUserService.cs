94b377a89eba5693034350e8262e60bc72d79ea0
Maycon Wisley Batista <maycon.batista@seniornortepr.com.br>
2024-09-20 16:19:04 -0300

    Ajuste

--- CalendarSchedule.Web/Service/ScheduleUserService.cs
+++ CalendarSchedule.Web/Service/ScheduleUserService.cs
@@ -286,7 +286,7 @@ public class ScheduleUserService : IScheduleUserService
 
             using var httpClient = _httpClientFactory.CreateClient("ConexaoApi");
             //httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.Bearer);
-            using var response = await httpClient.GetAsync($"{apiEndPoint}/DateStart/{dateStart.ToString("dd/MM/yyyy").Replace("/", "%2F")}/{dateEnd.ToString("dd/MM/yyyy").Replace("/", "%2F")}");
+            using var response = await httpClient.GetAsync($"{apiEndPoint}/Period/{dateStart.ToString("dd/MM/yyyy").Replace("/", "%2F")}/{dateEnd.ToString("dd/MM/yyyy").Replace("/", "%2F")}");
 
             if (response.IsSuccessStatusCode)
             {
