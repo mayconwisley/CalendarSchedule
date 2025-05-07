48d4a236c8e402de711000323d811127e285b518
Maycon Wisley <maycon.wisley@outlook.com>
2025-05-06 23:30:52 -0300

    Ajuste

--- CalendarSchedule.API/Program.cs
+++ CalendarSchedule.API/Program.cs
@@ -1,6 +1,3 @@
-using System.Text;
-using System.Text.Json.Serialization;
-using CalendarSchedule.Models.Abstractions;
 using CalendarSchedule.API.Data;
 using CalendarSchedule.API.Repository;
 using CalendarSchedule.API.Repository.Interface;
@@ -8,80 +5,86 @@ using CalendarSchedule.API.Service;
 using CalendarSchedule.API.Service.Interface;
 using CalendarSchedule.API.Utility;
 using CalendarSchedule.API.Utility.Interface;
+using CalendarSchedule.Models.Abstractions;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.Net.Http.Headers;
 using Microsoft.OpenApi.Models;
+using System.Text;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.Configure<ApiBehaviorOptions>(options =>
 {
-    options.SuppressModelStateInvalidFilter = true;
+	options.SuppressModelStateInvalidFilter = true;
 });
 builder.Services.AddControllers().AddJsonOptions(x =>
 {
-    x.JsonSerializerOptions.Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping;
-    x.JsonSerializerOptions.PropertyNamingPolicy = null;
-    x.JsonSerializerOptions.WriteIndented = true;
-    x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
-    x.JsonSerializerOptions.Encoder = System.Text.Encodings.Web.JavaScriptEncoder.Create(System.Text.Unicode.UnicodeRanges.All);
+	x.JsonSerializerOptions.Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping;
+	x.JsonSerializerOptions.PropertyNamingPolicy = null;
+	x.JsonSerializerOptions.WriteIndented = true;
+	x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+	x.JsonSerializerOptions.Encoder = System.Text.Encodings.Web.JavaScriptEncoder.Create(System.Text.Unicode.UnicodeRanges.All);
 });
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(
-    c =>
-    {
-        c.SwaggerDoc("v1", new OpenApiInfo
-        {
-            Title = "Agenda de Visitas a Clientes",
-            Version = "v1",
-            Description = "Documentação da API de Agenda",
-            Contact = new OpenApiContact
-            {
-                Name = "Maycon Wisley",
-                Url = new Uri("https://github.com/mayconwisley")
-            }
-        });
-        c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-        {
-            Name = "Authorization",
-            In = ParameterLocation.Header,
-            Type = SecuritySchemeType.ApiKey,
-            Scheme = "Bearer"
-        });
-        c.AddSecurityRequirement(new OpenApiSecurityRequirement()
-        {
-            {
-                new OpenApiSecurityScheme
-                {
-                    Reference = new OpenApiReference
-                    {
-                        Type = ReferenceType.SecurityScheme,
-                        Id = "Bearer"
-                    },
-                    Scheme = "oauth2",
-                    Name = "Bearer",
-                    In = ParameterLocation.Header,
-                },
-                new List<string>()
-            }
-        });
-    });
+	c =>
+	{
+		c.SwaggerDoc("v1", new OpenApiInfo
+		{
+			Title = "Agenda de Visitas a Clientes",
+			Version = "v1",
+			Description = "Documentação da API de Agenda",
+			Contact = new OpenApiContact
+			{
+				Name = "Maycon Wisley",
+				Url = new Uri("https://github.com/mayconwisley")
+			}
+		});
+		c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+		{
+			Name = "Authorization",
+			In = ParameterLocation.Header,
+			Type = SecuritySchemeType.ApiKey,
+			Scheme = "Bearer"
+		});
+		c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+		{
+			{
+				new OpenApiSecurityScheme
+				{
+					Reference = new OpenApiReference
+					{
+						Type = ReferenceType.SecurityScheme,
+						Id = "Bearer"
+					},
+					Scheme = "oauth2",
+					Name = "Bearer",
+					In = ParameterLocation.Header,
+				},
+				new List<string>()
+			}
+		});
+	});
 builder.Services.AddCors();
 
 var passDatabase = Environment.GetEnvironmentVariable("SQLPassword", EnvironmentVariableTarget.Machine);
 var userDB = Environment.GetEnvironmentVariable("SQLUser", EnvironmentVariableTarget.Machine);
 
 var strDataBase = builder.Configuration
-                 .GetConnectionString("ScheduleConect")!
-                 .Replace("{{pass}}", passDatabase)
-                 .Replace("{{userDB}}", userDB);
+				 .GetConnectionString("ScheduleConect")!
+				 .Replace("{{pass}}", passDatabase)
+				 .Replace("{{userDB}}", userDB);
 
-builder.Services.AddDbContext<ScheduleContext>(cd => cd.UseSqlServer(strDataBase));
+builder.Services.AddDbContext<ScheduleContext>(cd => cd.UseSqlServer(strDataBase, s =>
+{
+	s.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery);
+}));
 builder.Services.AddScoped<IScheduleRepository, ScheduleRepository>();
 builder.Services.AddScoped<IScheduleService, ScheduleService>();
 builder.Services.AddScoped<IScheduleUserRepository, ScheduleUserRepository>();
@@ -104,69 +107,69 @@ string strKey = builder.Configuration.GetSection("JWT")["Secret"]!;
 var key = Encoding.ASCII.GetBytes(strKey);
 builder.Services.AddAuthentication(opt =>
 {
-    opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-    opt.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+	opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+	opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+	opt.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
 })
-    .AddJwtBearer(opt =>
-    {
-        opt.Events = new JwtBearerEvents
-        {
-            OnChallenge = async context =>
-            {
-                context.HandleResponse();
-                context.Response.StatusCode = StatusCodes.Status401Unauthorized;
-                context.Response.ContentType = "application/json";
-                var result = Result.Failure(Error.Unauthorized("Token inválido ou expirado"));
-                await context.Response.WriteAsJsonAsync(result.Error);
-            },
-            OnForbidden = async context =>
-            {
-                context.Response.StatusCode = StatusCodes.Status403Forbidden;
-                context.Response.ContentType = "application/json";
-                var result = Result.Failure(Error.Forbidden("Acesso negado"));
-                await context.Response.WriteAsJsonAsync(result);
-            }
-        };
+	.AddJwtBearer(opt =>
+	{
+		opt.Events = new JwtBearerEvents
+		{
+			OnChallenge = async context =>
+			{
+				context.HandleResponse();
+				context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+				context.Response.ContentType = "application/json";
+				var result = Result.Failure(Error.Unauthorized("Token inválido ou expirado"));
+				await context.Response.WriteAsJsonAsync(result.Error);
+			},
+			OnForbidden = async context =>
+			{
+				context.Response.StatusCode = StatusCodes.Status403Forbidden;
+				context.Response.ContentType = "application/json";
+				var result = Result.Failure(Error.Forbidden("Acesso negado"));
+				await context.Response.WriteAsJsonAsync(result);
+			}
+		};
 
-        opt.RequireHttpsMetadata = false;
-        opt.SaveToken = true;
-        opt.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(key),
-            ValidateIssuer = false,
-            ValidateAudience = false
-        };
-    });
+		opt.RequireHttpsMetadata = false;
+		opt.SaveToken = true;
+		opt.TokenValidationParameters = new TokenValidationParameters
+		{
+			ValidateIssuerSigningKey = true,
+			IssuerSigningKey = new SymmetricSecurityKey(key),
+			ValidateIssuer = false,
+			ValidateAudience = false
+		};
+	});
 
 var app = builder.Build();
 app.Use(async (context, next) =>
 {
-    await next();
+	await next();
 
-    switch (context.Response.StatusCode)
-    {
-        case StatusCodes.Status405MethodNotAllowed:
-            context.Response.ContentType = "application/json";
-            var result = Result.Failure(Error.MethodNotAllowed("Metodo não implementado ou não permitido"));
-            await context.Response.WriteAsJsonAsync(result.Error);
-            break;
-        case StatusCodes.Status500InternalServerError:
-            context.Response.ContentType = "application/json";
-            var error = Result.Failure(Error.Internal("Erro interno no servidor"));
-            await context.Response.WriteAsJsonAsync(error.Error);
-            break;
-        default:
-            break;
-    }
+	switch (context.Response.StatusCode)
+	{
+		case StatusCodes.Status405MethodNotAllowed:
+			context.Response.ContentType = "application/json";
+			var result = Result.Failure(Error.MethodNotAllowed("Metodo não implementado ou não permitido"));
+			await context.Response.WriteAsJsonAsync(result.Error);
+			break;
+		case StatusCodes.Status500InternalServerError:
+			context.Response.ContentType = "application/json";
+			var error = Result.Failure(Error.Internal("Erro interno no servidor"));
+			await context.Response.WriteAsJsonAsync(error.Error);
+			break;
+		default:
+			break;
+	}
 });
 app.UseCors(policy => policy.WithOrigins("https://localhost:7296",
-                                         "http://192.168.0.102:5051",
-                                         "http://localhost:5051")
-    .AllowAnyMethod()
-    .AllowAnyHeader()
-    .WithHeaders(HeaderNames.ContentType)
+										 "http://192.168.0.102:5051",
+										 "http://localhost:5051")
+	.AllowAnyMethod()
+	.AllowAnyHeader()
+	.WithHeaders(HeaderNames.ContentType)
 );
 
 app.UseSwagger();
--- CalendarSchedule.Web/Service/ClientResponsibleService.cs
+++ CalendarSchedule.Web/Service/ClientResponsibleService.cs
@@ -1,208 +1,205 @@
-﻿using System.Net.Http.Headers;
-using System.Net.Http.Json;
-using System.Text;
-using System.Text.Json;
-using CalendarSchedule.Models.Abstractions;
+﻿using CalendarSchedule.Models.Abstractions;
 using CalendarSchedule.Models.Dtos;
 using CalendarSchedule.Web.Models;
 using CalendarSchedule.Web.Service.Interface;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
 
 namespace CalendarSchedule.Web.Service;
 
 public class ClientResponsibleService : IClientResponsibleService
 {
-    private readonly IHttpClientFactory _httpClientFactory;
-    private readonly ITokenStorageService _tokenStorageService;
-    private readonly JsonSerializerOptions _serializerOptions;
-    private const string? apiEndPoint = "api/ClientResponsible";
+	private readonly IHttpClientFactory _httpClientFactory;
+	private readonly ITokenStorageService _tokenStorageService;
+	private readonly JsonSerializerOptions _serializerOptions;
+	private const string? apiEndPoint = "api/ClientResponsible";
 
-    public ClientResponsibleService(IHttpClientFactory httpClientFactory, ITokenStorageService tokenStorageService)
-    {
-        _httpClientFactory = httpClientFactory;
-        _tokenStorageService = tokenStorageService;
-        _serializerOptions = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
-    }
+	public ClientResponsibleService(IHttpClientFactory httpClientFactory, ITokenStorageService tokenStorageService)
+	{
+		_httpClientFactory = httpClientFactory;
+		_tokenStorageService = tokenStorageService;
+		_serializerOptions = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+	}
 
-    public async Task<Result<ClientResponsibleDto>> Create(ClientResponsibleCreateDto clientResponsibleCreateDto)
-    {
-        try
-        {
-            var token = await _tokenStorageService.GetToken();
-            if (token.IsFailure)
-                return Result.Failure<ClientResponsibleDto>(token.Error);
+	public async Task<Result<ClientResponsibleDto>> Create(ClientResponsibleCreateDto clientResponsibleCreateDto)
+	{
+		try
+		{
+			var token = await _tokenStorageService.GetToken();
+			if (token.IsFailure)
+				return Result.Failure<ClientResponsibleDto>(token.Error);
 
-            using var httpClient = _httpClientFactory.CreateClient("ConexaoApi");
-            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.Value.Bearer);
+			using var httpClient = _httpClientFactory.CreateClient("ConexaoApi");
+			httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.Value.Bearer);
 
-            StringContent stringContent = new(JsonSerializer.Serialize(clientResponsibleCreateDto), Encoding.UTF8, "application/json");
+			StringContent stringContent = new(JsonSerializer.Serialize(clientResponsibleCreateDto), Encoding.UTF8, "application/json");
 
-            using var response = await httpClient.PostAsync(apiEndPoint, stringContent);
-            if ((response.IsSuccessStatusCode))
-            {
-                using Stream resApi = await response.Content.ReadAsStreamAsync();
+			using var response = await httpClient.PostAsync(apiEndPoint, stringContent);
+			if (response.IsSuccessStatusCode)
+			{
+				var clientResponsible = await response.Content.ReadFromJsonAsync<ClientResponsibleDto>(_serializerOptions);
+				if (clientResponsible is null)
+					return Result.Failure<ClientResponsibleDto>(Error.NotFound("Não econtrado"));
 
-                var clientResponsible = await JsonSerializer.DeserializeAsync<ClientResponsibleDto>(resApi, _serializerOptions);
-                if (clientResponsible is null)
-                    return Result.Failure<ClientResponsibleDto>(Error.NotFound("Não econtrado"));
+				return Result.Success(clientResponsible);
+			}
+			else
+			{
+				var error = await response.Content.ReadFromJsonAsync<Error>(_serializerOptions);
+				if (error is null)
+					return Result.Failure<ClientResponsibleDto>(Error.NotFound("Token não gerado"));
+				return Result.Failure<ClientResponsibleDto>(error);
+			}
+		}
+		catch (Exception ex)
+		{
+			return Result.Failure<ClientResponsibleDto>(Error.Internal($"Erro interno: {ex.Message}"));
+		}
+	}
 
-                return Result.Success(clientResponsible);
-            }
-            else
-            {
-                using Stream resApi = await response.Content.ReadAsStreamAsync();
-                var error = await JsonSerializer.DeserializeAsync<Error>(resApi, _serializerOptions);
-                if (error is null)
-                    return Result.Failure<ClientResponsibleDto>(Error.NotFound("Token não gerado"));
-                return Result.Failure<ClientResponsibleDto>(error);
-            }
-        }
-        catch (Exception ex)
-        {
-            return Result.Failure<ClientResponsibleDto>(Error.Internal($"Erro interno: {ex.Message}"));
-        }
-    }
+	public async Task<Result<bool>> Delete(int id)
+	{
+		try
+		{
+			var token = await _tokenStorageService.GetToken();
+			if (token.IsFailure)
+				return Result.Failure<bool>(token.Error);
 
-    public async Task<Result<bool>> Delete(int id)
-    {
-        try
-        {
-            var token = await _tokenStorageService.GetToken();
-            if (token.IsFailure)
-                return Result.Failure<bool>(token.Error);
+			using var httpClient = _httpClientFactory.CreateClient("ConexaoApi");
+			httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.Value.Bearer);
 
-            using var httpClient = _httpClientFactory.CreateClient("ConexaoApi");
-            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.Value.Bearer);
+			using var response = await httpClient.DeleteAsync($"{apiEndPoint}/{id}");
 
-            using var response = await httpClient.DeleteAsync($"{apiEndPoint}/{id}");
+			if (response.IsSuccessStatusCode)
+			{
+				return Result.Success(true);
+			}
+			else
+			{
+				using Stream resApi = await response.Content.ReadAsStreamAsync();
+				var error = await JsonSerializer.DeserializeAsync<Error>(resApi, _serializerOptions);
+				if (error is null)
+					return Result.Failure<bool>(Error.NotFound("Token não gerado"));
 
-            if (response.IsSuccessStatusCode)
-            {
-                return Result.Success(true);
-            }
-            else
-            {
-                using Stream resApi = await response.Content.ReadAsStreamAsync();
-                var error = await JsonSerializer.DeserializeAsync<Error>(resApi, _serializerOptions);
-                if (error is null)
-                    return Result.Failure<bool>(Error.NotFound("Token não gerado"));
+				return Result.Failure<bool>(error);
+			}
+		}
+		catch (Exception ex)
+		{
+			return Result.Failure<bool>(Error.Internal($"Erro interno: {ex.Message}"));
+		}
+	}
 
-                return Result.Failure<bool>(error);
-            }
-        }
-        catch (Exception ex)
-        {
-            return Result.Failure<bool>(Error.Internal($"Erro interno: {ex.Message}"));
-        }
-    }
+	public async Task<Result<PagedResultView<ClientResponsibleDto>>> GetAll(int page = 1, int size = 10, string search = "")
+	{
+		try
+		{
+			var token = await _tokenStorageService.GetToken();
+			if (token.IsFailure)
+				return Result.Failure<PagedResultView<ClientResponsibleDto>>(token.Error);
 
-    public async Task<Result<PagedResultView<ClientResponsibleDto>>> GetAll(int page = 1, int size = 10, string search = "")
-    {
-        try
-        {
-            var token = await _tokenStorageService.GetToken();
-            if (token.IsFailure)
-                return Result.Failure<PagedResultView<ClientResponsibleDto>>(token.Error);
+			using var httpClient = _httpClientFactory.CreateClient("ConexaoApi");
+			//httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.Value.Bearer);
 
-            using var httpClient = _httpClientFactory.CreateClient("ConexaoApi");
-            //httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.Value.Bearer);
+			using var response = await httpClient.GetAsync($"{apiEndPoint}/All?page={page}&size={size}&search={search}");
 
-            using var response = await httpClient.GetAsync($"{apiEndPoint}/All?page={page}&size={size}&search={search}");
+			if (response.IsSuccessStatusCode)
+			{
+				var clientResponsibleView = await response.Content.ReadFromJsonAsync<PagedResultView<ClientResponsibleDto>>(_serializerOptions);
+				if (clientResponsibleView is null)
+					return Result.Failure<PagedResultView<ClientResponsibleDto>>(Error.NotFound("Não econtrado"));
+				return Result.Success(clientResponsibleView);
+			}
+			else
+			{
+				using Stream resApi = await response.Content.ReadAsStreamAsync();
+				var error = await JsonSerializer.DeserializeAsync<Error>(resApi, _serializerOptions);
+				if (error is null)
+					return Result.Failure<PagedResultView<ClientResponsibleDto>>(Error.NotFound("Token não gerado"));
+				return Result.Failure<PagedResultView<ClientResponsibleDto>>(error);
+			}
+		}
+		catch (Exception ex)
+		{
+			return Result.Failure<PagedResultView<ClientResponsibleDto>>(Error.Internal($"Erro interno: {ex.Message}"));
+		}
+	}
 
-            if (response.IsSuccessStatusCode)
-            {
-                var clientResponsibleView = await response.Content.ReadFromJsonAsync<PagedResultView<ClientResponsibleDto>>(_serializerOptions);
-                if (clientResponsibleView is null)
-                    return Result.Failure<PagedResultView<ClientResponsibleDto>>(Error.NotFound("Não econtrado"));
-                return Result.Success(clientResponsibleView);
-            }
-            else
-            {
-                using Stream resApi = await response.Content.ReadAsStreamAsync();
-                var error = await JsonSerializer.DeserializeAsync<Error>(resApi, _serializerOptions);
-                if (error is null)
-                    return Result.Failure<PagedResultView<ClientResponsibleDto>>(Error.NotFound("Token não gerado"));
-                return Result.Failure<PagedResultView<ClientResponsibleDto>>(error);
-            }
-        }
-        catch (Exception ex)
-        {
-            return Result.Failure<PagedResultView<ClientResponsibleDto>>(Error.Internal($"Erro interno: {ex.Message}"));
-        }
-    }
+	public async Task<Result<ClientResponsibleDto>> GetById(int id)
+	{
+		try
+		{
+			var token = await _tokenStorageService.GetToken();
+			if (token.IsFailure)
+				return Result.Failure<ClientResponsibleDto>(token.Error);
 
-    public async Task<Result<ClientResponsibleDto>> GetById(int id)
-    {
-        try
-        {
-            var token = await _tokenStorageService.GetToken();
-            if (token.IsFailure)
-                return Result.Failure<ClientResponsibleDto>(token.Error);
+			using var httpClient = _httpClientFactory.CreateClient("ConexaoApi");
+			//httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.Value.Bearer);
 
-            using var httpClient = _httpClientFactory.CreateClient("ConexaoApi");
-            //httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.Value.Bearer);
+			using var response = await httpClient.GetAsync($"{apiEndPoint}/{id}");
 
-            using var response = await httpClient.GetAsync($"{apiEndPoint}/{id}");
+			if (response.IsSuccessStatusCode)
+			{
+				var clientResponsibleDto = await response.Content.ReadFromJsonAsync<ClientResponsibleDto>(_serializerOptions);
+				if (clientResponsibleDto is null)
+					return Result.Failure<ClientResponsibleDto>(Error.NotFound("Não econtrado"));
 
-            if (response.IsSuccessStatusCode)
-            {
-                var clientResponsibleDto = await response.Content.ReadFromJsonAsync<ClientResponsibleDto>(_serializerOptions);
-                if (clientResponsibleDto is null)
-                    return Result.Failure<ClientResponsibleDto>(Error.NotFound("Não econtrado"));
+				return Result.Success(clientResponsibleDto);
+			}
+			else
+			{
+				using Stream resApi = await response.Content.ReadAsStreamAsync();
+				var error = await JsonSerializer.DeserializeAsync<Error>(resApi, _serializerOptions);
+				if (error is null)
+					return Result.Failure<ClientResponsibleDto>(Error.NotFound("Token não gerado"));
 
-                return Result.Success(clientResponsibleDto);
-            }
-            else
-            {
-                using Stream resApi = await response.Content.ReadAsStreamAsync();
-                var error = await JsonSerializer.DeserializeAsync<Error>(resApi, _serializerOptions);
-                if (error is null)
-                    return Result.Failure<ClientResponsibleDto>(Error.NotFound("Token não gerado"));
+				return Result.Failure<ClientResponsibleDto>(error);
+			}
+		}
+		catch (Exception ex)
+		{
+			return Result.Failure<ClientResponsibleDto>(Error.Internal($"Erro interno: {ex.Message}"));
+		}
+	}
 
-                return Result.Failure<ClientResponsibleDto>(error);
-            }
-        }
-        catch (Exception ex)
-        {
-            return Result.Failure<ClientResponsibleDto>(Error.Internal($"Erro interno: {ex.Message}"));
-        }
-    }
+	public async Task<Result<ClientResponsibleDto>> Update(ClientResponsibleDto clientResponsibleDto)
+	{
+		try
+		{
+			var token = await _tokenStorageService.GetToken();
+			if (token.IsFailure)
+				return Result.Failure<ClientResponsibleDto>(token.Error);
 
-    public async Task<Result<ClientResponsibleDto>> Update(ClientResponsibleDto clientResponsibleDto)
-    {
-        try
-        {
-            var token = await _tokenStorageService.GetToken();
-            if (token.IsFailure)
-                return Result.Failure<ClientResponsibleDto>(token.Error);
+			using var httpClient = _httpClientFactory.CreateClient("ConexaoApi");
+			httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.Value.Bearer);
 
-            using var httpClient = _httpClientFactory.CreateClient("ConexaoApi");
-            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.Value.Bearer);
+			StringContent stringContent = new(JsonSerializer.Serialize(clientResponsibleDto), Encoding.UTF8, "application/json");
 
-            StringContent stringContent = new(JsonSerializer.Serialize(clientResponsibleDto), Encoding.UTF8, "application/json");
+			using var response = await httpClient.PutAsync($"{apiEndPoint}/{clientResponsibleDto.Id}", stringContent);
 
-            using var response = await httpClient.PutAsync($"{apiEndPoint}/{clientResponsibleDto.Id}", stringContent);
+			if (response.IsSuccessStatusCode)
+			{
+				using Stream resApi = await response.Content.ReadAsStreamAsync();
+				var clientResponsible = await JsonSerializer.DeserializeAsync<ClientResponsibleDto>(resApi, _serializerOptions);
+				if (clientResponsible is null)
+					return Result.Failure<ClientResponsibleDto>(Error.NotFound("Não econtrado"));
 
-            if (response.IsSuccessStatusCode)
-            {
-                using Stream resApi = await response.Content.ReadAsStreamAsync();
-                var clientResponsible = await JsonSerializer.DeserializeAsync<ClientResponsibleDto>(resApi, _serializerOptions);
-                if (clientResponsible is null)
-                    return Result.Failure<ClientResponsibleDto>(Error.NotFound("Não econtrado"));
-
-                return Result.Success(clientResponsible);
-            }
-            else
-            {
-                using Stream resApi = await response.Content.ReadAsStreamAsync();
-                var error = await JsonSerializer.DeserializeAsync<Error>(resApi, _serializerOptions);
-                if (error is null)
-                    return Result.Failure<ClientResponsibleDto>(Error.NotFound("Token não gerado"));
-                return Result.Failure<ClientResponsibleDto>(error);
-            }
-        }
-        catch (Exception ex)
-        {
-            return Result.Failure<ClientResponsibleDto>(Error.Internal($"Erro interno: {ex.Message}"));
-        }
-    }
+				return Result.Success(clientResponsible);
+			}
+			else
+			{
+				using Stream resApi = await response.Content.ReadAsStreamAsync();
+				var error = await JsonSerializer.DeserializeAsync<Error>(resApi, _serializerOptions);
+				if (error is null)
+					return Result.Failure<ClientResponsibleDto>(Error.NotFound("Token não gerado"));
+				return Result.Failure<ClientResponsibleDto>(error);
+			}
+		}
+		catch (Exception ex)
+		{
+			return Result.Failure<ClientResponsibleDto>(Error.Internal($"Erro interno: {ex.Message}"));
+		}
+	}
 }
